2af4c3d7787b251223f5a41301f65b5477ef4ca1
Alex Navarro <lexn29@gmail.com>
2023-02-14 22:09:50 -0600

    created notFound component

---
+++ src/modules/common/components/NotFound/NotFound.tsx
@@ -0,0 +1,12 @@
+import styles from './not-found.module.scss'
+
+const NotFound = () => {
+  return (
+    <div className={styles.container}>
+      <h1>404</h1>
+      <p>Page Not Found</p>
+    </div>
+  )
+}
+
+export default NotFound
