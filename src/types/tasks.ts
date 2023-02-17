83fe3d30c9f50085a810c6de0f5dbbab97173341
Alex Navarro <lexn29@gmail.com>
2023-02-17 02:02:18 -0600

    created types for tasks

--- src/types/tasks.ts
+++ src/types/tasks.ts
@@ -1,1 +1,33 @@
 export type ViewToggle = 'grid' | 'list'
+
+export type TaskStatus = 'BACKLOG' | 'CANCELLED' | 'DONE' | 'IN_PROGRESS' | 'TODO'
+
+export type PointEstimate = 'ZERO' | 'ONE' | 'TWO' | 'FOUR' | 'EIGHT'
+
+export type DueDateStatus = 'ON_TIME' | 'ALMOST_LATE' | 'LATE'
+
+export interface DueDateInfo {
+  formattedDate: string
+  date: string
+  status: DueDateStatus
+}
+
+export interface Task {
+  assignee: {
+    avatar: string
+    createdAt: Date
+    email: string
+    fullName: string
+    id: string
+    type: string
+    updatedAt: Date
+  }
+  createdAt: Date
+  dueDate: string
+  id: string
+  name: string
+  pointEstimate: PointEstimate
+  position: number
+  status: TaskStatus
+  tags: string[]
+}
